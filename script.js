2344371a6f198a0644c98ae47a787335306e2842
Joswui <castrojoswui@gmail.com>
2023-10-27 19:45:38 -0400

    Agregar .gitignore primeros archivos del proyecto

---
+++ script.js
@@ -0,0 +1,16 @@
+const h1 = document.querySelector('h1');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular');
+const pResult = document.querySelector('#result');
+
+btn.addEventListener('click', btnOnclick);
+
+    function btnOnclick () {
+    const sumaInputs = input1.value + input2.value;
+    pResult.innerText = "Resultado: " + sumaInputs;
+}
+//function btnOnclick() {
+    //const sumaInputs = parseInt(input1.value) + parseInt(input2.value);
+    //pResult.innerText = "Resultado "+sumaInputs;
+//}
